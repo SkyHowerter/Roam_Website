1bf58f14b7f753facd18ccb0107047f4dda6907d
atsalyuk <atsalyuk@gmail.com>
2019-04-17 15:23:35 -0600

    Basic country display functionality implemented

--- server.js
+++ server.js
@@ -17,17 +17,32 @@ var data;
 var query = "select * from roam;";
 
 db.any(query).then(function (rows){
-	data = rows;
+  data = rows;
 });
 app.set('view engine', 'ejs');
 app.get('/', function(req,res){
+  var sliders;
+  var modal;
   if(req.query){
-    var sliders = [req.query.s0, req.query.s1, req.query.s2, req.query.s3, req.query.s4];
-    res.render('index', {slides: sliders});
-    console.log(req.query);
+    sliders = [req.query.s0, req.query.s1, req.query.s2, req.query.s3, req.query.s4];
+    max = 0;
+    for(i = 0; i < Object.keys(data).length; i++){
+      var num = (6 - Math.abs(sliders[0]/20 - data[i].touristy))/6;
+      num *= (6 - Math.abs(sliders[1]/20 - data[i].luxury))/6;
+      num *= (6 - Math.abs(sliders[2]/20 - data[i].expense))/6;
+      num *= (6 - Math.abs(sliders[3]/20 - data[i].popage))/6;
+      num *= (6 - Math.abs(sliders[4]/20 - data[i].faraway))/6;
+      console.log(Math.abs(sliders[0]/20 - data[i].touristy));
+      if(max < num){
+        max = num;
+        modal = data[i].location;
+      }
+    }
+    console.log(modal);
   } else{
-    res.render('index');
+    sliders = [50,50,50,50,50];
   }
+  res.render('index', {slides: sliders, modal: modal});
 });
 
 app.post('/',function(req,res){
